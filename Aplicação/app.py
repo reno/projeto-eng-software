19ff8f717ba94a16ee57701cb0888cf367752d4e
reno <renan.modenese@gmail.com>
2019-05-29 20:43:19 -0300

    Incluindo aplicação base

--- Aplicação/app.py
+++ Aplicação/app.py
@@ -1,7 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
 @app.route('/')
 def index():
-    return '<h1>Olá!</h1>'
+    return render_template('index.html')
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
