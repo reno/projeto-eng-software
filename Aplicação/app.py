2200a23570f5665bd0c17af3896fa8403ef66f91
reno <renan.modenese@gmail.com>
2019-05-29 23:26:58 -0300

    Corrigindo definições do BD

--- Aplicação/app.py
+++ Aplicação/app.py
@@ -1,15 +1,14 @@
-import os
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from models import *
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://localhost/livraria'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 db = SQLAlchemy(app)
-
-app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db.init_app(app)
 
 
@@ -20,10 +19,3 @@ def index():
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
-
-def main():
-    db.create_all()
-
-if __name__ == "__main__":
-    with app.app_context():
-        main()
