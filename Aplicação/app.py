ec76893260f8fe5a95aca12b60c327e0dbeda587
reno <renan.modenese@gmail.com>
2019-05-29 23:03:45 -0300

    Incluindo definição da tabela Livro

--- Aplicação/app.py
+++ Aplicação/app.py
@@ -1,8 +1,17 @@
+import os
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+from models import *
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
+db = SQLAlchemy(app)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db.init_app(app)
+
 
 @app.route('/')
 def index():
@@ -11,3 +20,10 @@ def index():
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
+
+def main():
+    db.create_all()
+
+if __name__ == "__main__":
+    with app.app_context():
+        main()
---
+++ Aplicação/models.py
@@ -0,0 +1,18 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Livro(db.Model):
+    __tablename__ = "livros"
+    id = db.Column(db.Integer, primary_key=True)
+    titulo = db.Column(db.String, nullable=False)
+    autor = db.Column(db.String, nullable=False)
+    editora = db.Column(db.String, nullable=False)
+    edicao = db.Column(db.String, nullable=False)
+    ano = db.Column(db.Integer, nullable=False)
+    isbn = db.Column(db.String, nullable=False, unique=True)
+    preco = db.Column(db.Float, nullable=False)
+    exemplares = db.Column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        return '<Livro %r>' % self.titulo
