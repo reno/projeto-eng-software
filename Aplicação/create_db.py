52c8f15a01d5414c81cc7439ddc2b3e59fa5a9da
reno <renan.modenese@gmail.com>
2019-05-30 22:48:05 -0300

    Refatorando

--- Aplicação/create_db.py
+++ Aplicação/create_db.py
@@ -2,19 +2,17 @@ import os
 from flask import Flask
 from models import *
 
-basedir = os.path.abspath(os.path.dirname(__file__))
-
+app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://localhost/livraria'
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-app = Flask(__name__)
+# db é inicializado em models.py 
 db.init_app(app)
 
 def main():
     db.create_all()
+    print('BD criado com sucesso.')
 
 if __name__ == "__main__":
     with app.app_context():
         main()
-
-
