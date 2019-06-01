f2fcd0c83a2fbefd35db22bd120dd0c0aba4231a
reno <renan.modenese@gmail.com>
2019-05-31 23:17:15 -0300

    Incluindo form de atualização de livros

--- Aplicação/forms.py
+++ Aplicação/forms.py
@@ -6,7 +6,7 @@ from wtforms.validators import InputRequired as Req, Regexp, Email
 class FormConsultaLivro(FlaskForm):
     campo = SelectField('Campo de busca', choices=[('isbn','ISBN'), ('titulo','Título')])
     termo = StringField('Palavra-chave', validators=[Req()])
-    submit = SubmitField()
+    submit = SubmitField('Consultar')
 
 
 class FormCadastroLivro(FlaskForm):
@@ -19,8 +19,20 @@ class FormCadastroLivro(FlaskForm):
     idioma = StringField('Idioma', validators=[Req()])
     preco = DecimalField('Preço', validators=[Req()])
     exemplares = IntegerField('Número de exemplares', validators=[Req()])
-    submit = SubmitField()
+    submit = SubmitField('Cadastrar')
 
-#class FormAtualizacaoLivro(FlaskForm):
+class FormAtualizacaoLivro(FlaskForm):
+    titulo = StringField('Título', validators=[Req()])
+    autor = StringField('Autor', validators=[Req()])
+    editora = StringField('Editora', validators=[Req()])
+    edicao = StringField('Edição', validators=[Req()])
+    ano = IntegerField('Ano', validators=[Req()])
+    isbn = StringField('ISBN', validators=[Req()])
+    idioma = StringField('Idioma', validators=[Req()])
+    preco = DecimalField('Preço', validators=[Req()])
+    exemplares = IntegerField('Número de exemplares', validators=[Req()])
+    submit = SubmitField('Cadastrar')
 
-#class FormExclusaoLivro(FlaskForm):
+class FormExclusaoLivro(FlaskForm):
+    isbn = StringField('ISBN', validators=[Req()])
+    submit = SubmitField('Excluir')
