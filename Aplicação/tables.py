755d5b226b8214a9c9a24a89a949c0a5f081be03
reno <renan.modenese@gmail.com>
2019-05-31 21:03:47 -0300

    Definindo tabelas para exibir dados

---
+++ Aplicação/tables.py
@@ -0,0 +1,12 @@
+from flask_table import Table, Col
+
+class TabelaLivros(Table):
+    id = Col('ID', show=False)
+    isbn = Col('ISBN')
+    titulo = Col('Título')
+    autor = Col('Autor')
+    editora = Col('Editora')
+    edicao = Col('Edição')
+    ano = Col('Ano')
+    preco = Col('Preço')
+    exemplares = Col('Exemplares')
