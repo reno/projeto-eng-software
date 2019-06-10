677855ad0b1c475f8e9215a1c87e0a2130b52047
reno <renan.modenese@gmail.com>
2019-06-10 20:14:32 -0300

    Adicionando função test()
    Executa automaticamente testes unitários. Uso: $ flask test

--- Aplicação/app.py
+++ Aplicação/app.py
@@ -4,10 +4,25 @@ app.py
 Instancia app, para desenvolvimento. Recebe argumento ('dev', 'testes' ou 'producao').
 '''
 
+import click
 from sys import argv
 import livraria
-if argv[1]: livraria.set_config(argv[1])
+#if argv[1]: livraria.set_config(argv[1])
+livraria.set_config('base')
 from livraria import app
+
+
+@app.cli.command()
+@click.argument('test_names', nargs=-1)
+def test(test_names):
+    """Executa os testes unitários"""
+    import unittest
+    if test_names:
+        tests = unittest.TestLoader().loadTestsFromNames(test_names)
+    else:
+        tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
