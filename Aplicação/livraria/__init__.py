5a538b211c05aeab83c0ba140e8331319adfa0e4
reno <renan.modenese@gmail.com>
2019-06-12 11:47:19 -0300

    Implementando validação de usuário

--- Aplicação/livraria/__init__.py
+++ Aplicação/livraria/__init__.py
@@ -6,7 +6,12 @@ Inicialização do pacote livraria, executado na importação.
 
 from flask import Flask
 from flask_bootstrap import Bootstrap
+from flask_login import LoginManager
 from config import config
+
+login_manager = LoginManager()
+login_manager.login_view = 'login'
+login_manager.login_message = 'Entre com seu usuário para acessar esta página.'
 
 def set_config(ambiente):
     '''Sobrescreve as configurações base conforme o ambiente'''
@@ -14,8 +19,9 @@ def set_config(ambiente):
     bootstrap = Bootstrap(app)
     from .models import db
     db.init_app(app)
+    login_manager.init_app(app)
 
+# executado na importação:
 app = Flask(__name__)
 app.config.from_object(config['base'])
-#from .views import *
-from .views import livro, erros
+from .views import principal, livro
