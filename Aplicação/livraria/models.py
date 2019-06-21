eb85615df0fd9f84cbe1175603a1e033997d4b17
reno <renan.modenese@gmail.com>
2019-06-20 23:08:28 -0300

    Incluindo definições de cliente e pedido

--- Aplicação/livraria/models.py
+++ Aplicação/livraria/models.py
@@ -1,20 +1,17 @@
 '''
 livraria/models.py
 
-Define tabelas do banco de dados. Para criação do BD, execute o script 'create_db.py'
+Define tabelas do banco de dados.
+Para criação do BD, execute o script 'create_db.py'
 '''
 
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.ext.hybrid import hybrid_property
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from . import login_manager
 
 db = SQLAlchemy()
-
-@login_manager.user_loader
-def load_user(registro):
-    return Funcionario.query.get(int(registro))
-
 
 class Livro(db.Model):
     __tablename__ = "livros"
@@ -32,9 +29,6 @@ class Livro(db.Model):
     def __repr__(self):
         return self.isbn
 
-
-ADMIN = True
-VENDEDOR = False
 
 class Funcionario(UserMixin, db.Model):
     __tablename__ = "funcionarios"
@@ -62,16 +56,75 @@ class Funcionario(UserMixin, db.Model):
     def __repr__(self):
         return self.usuario
 
+ADMIN = True
+VENDEDOR = False
 
 class Vendedor(Funcionario):
     __mapper_args__ = {'polymorphic_identity': VENDEDOR}
 
-
 class Admin(Funcionario):
     __mapper_args__ = {'polymorphic_identity': ADMIN}
-
-# consultar usando query(Vendedor).filter_by() ou query.with_polymorphic()
 
 @login_manager.user_loader
 def load_user(user_id):
     return Funcionario.query.get(int(user_id))
+
+
+class Cliente(db.Model):
+    __tablename__ = "clientes"
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String, nullable=False)
+    documento = db.Column(db.String, nullable=False)
+    data_nascimento = db.Column(db.Date, nullable=False)
+    endereco = db.relationship('Endereco', cascade="all,delete", backref='cliente', lazy=True, uselist=False)
+    telefone = db.Column(db.String, nullable=False)
+    email = db.Column(db.String, nullable=False)
+
+    def __repr__(self):
+        return self.documento
+
+
+class Endereco(db.Model):
+    __tablename__ = "enderecos"
+    id = db.Column(db.Integer, primary_key=True)
+    logradouro = db.Column(db.String, nullable=False)
+    numero = db.Column(db.String, nullable=False)
+    complemento = db.Column(db.String)
+    bairro = db.Column(db.String, nullable=False)
+    cidade = db.Column(db.String, nullable=False)
+    estado = db.Column(db.String, nullable=False)
+    cep = db.Column(db.String, nullable=False)
+    id_cliente = db.Column(db.Integer, db.ForeignKey('clientes.id'), nullable=False)
+    
+
+itens_pedido = db.Table('itens_pedido',
+    db.Column('id_livro', db.Integer, db.ForeignKey('livros.id'), primary_key=True),
+    db.Column('quantidade', db.Integer),
+    db.Column('id_pedido', db.Integer, db.ForeignKey('pedidos.id'), primary_key=True)
+)
+
+
+class Pedido(db.Model):
+    __tablename__ = "pedidos"
+    id = db.Column(db.Integer, primary_key=True)
+    itens = db.relationship('Livro', secondary=itens_pedido, lazy='subquery',
+                            backref=db.backref('pedidos', lazy=True))
+    #id_item = db.Column(db.ARRAY(db.Integer), db.ForeignKey('itens_pedido.id'))
+    id_cliente = db.Column(db.Integer, db.ForeignKey('clientes.id'))
+    id_vendedor = db.Column(db.Integer, db.ForeignKey('funcionarios.id'))
+    desconto = db.Column(db.Float, nullable=False)
+    _valor_total = db.Column(db.Float, nullable=False)
+    ativo = db.Column(db.Boolean)
+
+    '''
+    @hybrid_property
+    def valor_total(self):
+        _valor_total = sum([valor_item(i) for i in id_item])
+
+    #@valor_item.expression
+    def valor_item(id_item):
+        return ItemPedido.query.get(id_item).quantidade * Livro.query.get(id_item).preco
+    '''
+    def __repr__(self):
+        return self.id
+
