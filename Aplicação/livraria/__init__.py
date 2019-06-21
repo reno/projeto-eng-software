e0f2391b764285e049bd4242c3445098f2bab783
reno <renan.modenese@gmail.com>
2019-06-20 23:10:16 -0300

    Incluindo inicialização do LoginManager

--- Aplicação/livraria/__init__.py
+++ Aplicação/livraria/__init__.py
@@ -7,21 +7,29 @@ Inicialização do pacote livraria, executado na importação.
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask_login import LoginManager
+from flask_wtf.csrf import CSRFProtect
 from config import config
+
+def set_config(ambiente):
+    '''Sobrescreve as configurações base conforme o ambiente'''
+    app.config.from_object(config[ambiente])
 
 login_manager = LoginManager()
 login_manager.login_view = 'login'
 login_manager.login_message = 'Entre com seu usuário para acessar esta página.'
 
-def set_config(ambiente):
-    '''Sobrescreve as configurações base conforme o ambiente'''
-    app.config.from_object(config[ambiente])
-    bootstrap = Bootstrap(app)
-    from .models import db
-    db.init_app(app)
-    login_manager.init_app(app)
-
 # executado na importação:
 app = Flask(__name__)
 app.config.from_object(config['base'])
-from .views import principal, livro, admin
+from .views import principal, livro, pedido, cliente, admin
+
+app.config['WTF_CSRF_SECRET_KEY'] = 'string de verificacao do flask-wtf'
+
+csrf = CSRFProtect(app)
+login_manager.init_app(app)
+bootstrap = Bootstrap(app)
+
+from .models import db
+db.init_app(app)
+
+
