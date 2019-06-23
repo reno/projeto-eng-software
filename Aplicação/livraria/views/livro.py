659662e78a6cd8021b41ad13acbf8b70edaa9f1f
reno <renan.modenese@gmail.com>
2019-06-23 18:02:12 -0300

    Atualizando rotas do menu Livro

--- Aplicação/livraria/views/livro.py
+++ Aplicação/livraria/views/livro.py
@@ -10,7 +10,7 @@ from isbnlib import is_isbn10, is_isbn13, mask, canonical, meta
 from livraria.models import *
 from livraria.tables import *
 from livraria.forms.livro import *
-from livraria import app #, views
+from livraria import app
 
 
 @app.route('/livro/consultar', methods=['GET', 'POST'])
@@ -25,7 +25,7 @@ def consultar_livro():
         return render_template('livro/resultado.html', table=tabela)
     # formulário ainda não enviado, renderiza página
     else:
-        return render_template('livro/livro.html', form=form, header='Consultar livro')
+        return render_template('livro/consultar.html', form=form, header='Consultar livro')
 
 
 @app.route('/livro/cadastrar', methods=['GET', 'POST'])
@@ -36,15 +36,15 @@ def cadastrar_livro():
     if form.validate_on_submit():
         livro = Livro.query.filter_by(isbn=form.isbn.data).first()
         if livro is not None:
-            return render_template('index.html', text='Livro {} já cadastrado.'.format(mask(str(livro))))
+            return render_template('livro/index.html', text='Livro {} já cadastrado.'.format(mask(str(livro))))
         else:
             dados = {k: v for k, v in form.data.items()
                      if k not in {'submit','csrf_token'}}
             livro = Livro(**dados)
-            livro['isbn'] = canonical(livro['isbn'])
+            #livro['isbn'] = canonical(livro['isbn'])
             db.session.add(livro)
             db.session.commit()
-            return render_template('index.html', text='Livro {} cadastrado com sucesso.'.format(mask(livro)))
+            return render_template('livro/index.html', text='Livro {} cadastrado com sucesso.'.format(mask(str(livro))))
     # formulário ainda não enviado, renderiza página
     else:
         return render_template('livro/cadastrar.html', form=form, header='Cadastrar livro')
@@ -73,14 +73,14 @@ def consultar_isbn(op):
     if form_isbn.validate_on_submit():
         livro = Livro.query.filter_by(isbn=form_isbn.isbn.data).first()
         if livro is None:
-            return render_template('index.html', text='Nenhum livro encontrado.')    
+            return render_template('livro/index.html', text='Nenhum livro encontrado.')    
         if op == 'atualizar':
             return redirect(url_for('atualizar_livro', isbn=livro.isbn))
         else:
             return redirect(url_for('excluir_livro', isbn=livro.isbn))
     # formulário ainda não enviado, renderiza página
     else: 
-        return render_template('livro/livro.html', form=form_isbn, header='{} livro'.format(op.capitalize()))
+        return render_template('livro/isbn.html', form=form_isbn, header='{} livro'.format(op.capitalize()))
 
     
 @app.route('/livro/atualizar/', methods=['GET', 'POST'])
@@ -95,10 +95,10 @@ def atualizar_livro():
                  if k not in {'submit','csrf_token'}}
         livro.data = dados 
         db.session.commit()
-        return render_template('index.html', text='Livro atualizado com sucesso.')  
+        return render_template('livro/index.html', text='Livro atualizado com sucesso.')  
     # formulário ainda não enviado, renderiza página
     else:
-        return render_template('livro/livro.html', form=form_atualizacao) 
+        return render_template('livro/atualizar.html', form=form_atualizacao, header='Atualizar livro') 
 
 
 @app.route('/livro/excluir/', methods=['GET', 'POST'])
@@ -112,6 +112,6 @@ def excluir_livro():
     if confirmacao.validate_on_submit():
         db.session.delete(livro)
         db.session.commit()
-        return render_template('index.html', text='Livro excluido com sucesso.')
+        return render_template('livro/index.html', text='Livro excluido com sucesso.')
     else:
         return render_template('livro/excluir.html', table=tabela, form=confirmacao, header='Excluir livro')
