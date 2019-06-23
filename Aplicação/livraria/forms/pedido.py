00aeac4a699f6df0a7edc2cf1d8148e1701ec8d0
reno <renan.modenese@gmail.com>
2019-06-23 18:00:29 -0300

    Corrigindo forms pedido

--- Aplicação/livraria/forms/pedido.py
+++ Aplicação/livraria/forms/pedido.py
@@ -5,7 +5,7 @@ Define formularios usados no menu Pedidos.
 '''
 
 from flask_wtf import FlaskForm
-from wtforms import IntegerField, DecimalField, StringField, SubmitField, SelectField, FieldList, FormField
+from wtforms import IntegerField, DecimalField, StringField, SubmitField, SelectField, FieldList, FormField, BooleanField, HiddenField
 from wtforms.validators import DataRequired as Data, Email, Regexp, ValidationError
 from isbnlib import is_isbn10, is_isbn13
 from livraria.models import *
@@ -18,31 +18,28 @@ class FormItensPedido(FlaskForm):
     def validate_livro(self, field):
         if not is_isbn10(field.data) and not is_isbn13(field.data):
             return ValidationError('ISBN inválido')
-        if not Livro.query.filter_by(isbn=field.data).first():
+        if Livro.query.filter_by(isbn=field.data).first() is None:
             raise ValidationError('Livro não encontrado.')
 
 
 class FormCadastroPedido(FlaskForm):
     cliente = StringField('Cliente', validators=[Data()])
-    itens = FieldList(FormField(FormItensPedido), min_entries=1, max_entries=99)
-    vendedor = StringField('Vendedor', validators=[Data()])
+    itens = FieldList(FormField(FormItensPedido), min_entries=1, max_entries=9)
+    #vendedor = HiddenField()
     desconto = StringField('Desconto', validators=[Data()])
-    total = StringField('Valor total', validators=[Data()])
+    #total = HiddenField()
+    submit = SubmitField('Cadastrar')
 
     def validate_cliente(self, field):
-        if not Cliente.query.filter_by(documento=field.data).first():
+        if Cliente.query.filter_by(documento=field.data).first() is None:
             raise ValidationError('Cliente não encontrado.')
-
-    def validate_vendedor(self, field):
-        if not Funcionario.query.filter_by(id=field.data).first():
-            raise ValidationError('Vendedor não encontrado')
 
 
 class FormConsultaPedido(FlaskForm):
-    opcoes = [(atr, atr.capitalize()) for atr in dir(Pedido)
-              if atr is not 'id' and not atr.startswith(('_'))]
+    opcoes = [('id', 'Nº pedido'), ('cliente', 'Cliente'), ('vendedor', 'Vendedor')]
     campo = SelectField('Campo de busca', choices=opcoes)  
     termo = StringField('Palavra-chave', validators=[Data()])
+    consultar_ativos = BooleanField('Consultar somente pedidos ativos.')
     submit = SubmitField('Consultar')
 
 
@@ -51,6 +48,6 @@ class FormNumeroPedido(FlaskForm):
     submit = SubmitField('Consultar')
 
 
-class FormExclusaoPedido(FlaskForm):
+class FormCancelamentoPedido(FlaskForm):
     submit = SubmitField('Excluir')
 
