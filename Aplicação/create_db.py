d62155da7f1f2f693397c28ae355122e90439c38
reno <renan.modenese@gmail.com>
2019-06-23 17:59:27 -0300

    Incluindo inserções para teste do sistema

--- Aplicação/create_db.py
+++ Aplicação/create_db.py
@@ -17,7 +17,17 @@ def main():
     db.drop_all()
     db.create_all()
     admin = Funcionario(nome='Administrador', usuario='admin', senha='admin', admin=True)
+    vendedor = Vendedor(nome='Vendedor', usuario='vendedor', senha='1234')
+    endereco = Endereco(logradouro='rua', numero='sn', bairro='bairro',
+                        cidade='Lavras', estado='MG', cep= '37200000')
+    cliente = Cliente(nome='Cliente', documento='12345678', data_nascimento='01/01/1990',
+                      endereco=endereco, telefone='3598765432', email='cliente@dominio.com')
+    livro = Livro(titulo='livro', autor='autor', editora='editora', edicao=1,
+                  ano='2019', isbn='1', idioma='Pt', preco=9.99, exemplares=10)
     db.session.add(admin)
+    db.session.add(vendedor)
+    db.session.add(cliente)
+    db.session.add(livro)
     db.session.commit()
     print('BD criado com sucesso.')
 
