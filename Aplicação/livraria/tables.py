b5fd179d651be7597700c80a860c48cc1221e07d
reno <renan.modenese@gmail.com>
2019-06-23 18:01:19 -0300

    Incluindo tabela de pedidos

--- Aplicação/livraria/tables.py
+++ Aplicação/livraria/tables.py
@@ -4,7 +4,7 @@ livraria/tables.py
 Define tabelas usadas no resultado das consultas.
 '''
 
-from flask_table import Table, Col
+from flask_table import Table, Col, DateCol, NestedTableCol
 from isbnlib import mask
 
 
@@ -16,6 +16,11 @@ class IsbnCol(Col):
             return content
 
 
+class TitleCol(Col):
+    def td_format(self, content):
+        return '{}'.format(content.titulo) 
+
+
 class PriceCol(Col):
     def td_format(self, content):
         return 'R$ {:5.2f}'.format(content)
@@ -25,9 +30,18 @@ class EditionCol(Col):
     def td_format(self, content):
         return '{}ª'.format(content)
 
+class ActiveCol(Col):
+    def td_format(self, content):
+        if content is True:
+            return 'Ativo'
+        else:
+            return 'Cancelado'
+
+
 
 class TabelaLivros(Table):
     classes = ['table', 'table-striped', 'col-lg-3']
+    no_items = 'Nenhum livro encontrado.'
     id = Col('ID', show=False)
     isbn = IsbnCol('ISBN')
     titulo = Col('Título')
@@ -41,10 +55,11 @@ class TabelaLivros(Table):
 
 class TabelaClientes(Table):
     classes = ['table', 'table-striped', 'col-lg-3']
+    no_items = 'Nenhum cliente encontrado.'
     id = Col('ID', show=False)
     nome = Col('Nome')
     documento = Col('Documento')
-    data_nascimento = Col('Data de nasc.')
+    data_nascimento = DateCol('Data de nasc.')
     endereco = Col('Endereço')
     telefone = Col('Telefone')
     email = Col('E-mail')
@@ -52,12 +67,23 @@ class TabelaClientes(Table):
 
 class TabelaFuncionarios(Table):
     classes = ['table', 'table-striped', 'col-lg-3']
+    no_items = 'Nenhum funcionário encontrado.'
     id = Col('Registro')
     nome = Col('Nome')
     usuario = Col('Usuário')
 
     
+class TabelaItens(Table):
+    livro = TitleCol('Livro')
+    quantidade = Col('Qnt.')
+
 class TabelaPedidos(Table):
     classes = ['table', 'table-striped', 'col-lg-3']
-    id = Col('ID', show=False)
-    pass
+    no_items = 'Nenhum pedido encontrado.'
+    id = Col('Nº pedido')
+    cliente = Col('Cliente')
+    vendedor = Col('Vendedor')
+    itens = NestedTableCol('Itens', TabelaItens)
+    desconto = PriceCol('Desconto')
+    total = PriceCol('Total')
+    ativo = ActiveCol('Status')
