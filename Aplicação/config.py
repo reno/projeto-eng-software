53bfe8eb14ae85a7949748a5ad53a8bff0ba7fb2
reno <renan.modenese@gmail.com>
2019-06-28 17:03:26 -0300

    Atualizando configurações

--- Aplicação/config.py
+++ Aplicação/config.py
@@ -1,14 +1,15 @@
-'''
+"""
 config.py
 
 Define configurações para diferentes ambientes.
-'''
+"""
 
 import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 SQLLITE = 'sqlite:///' + os.path.join(basedir, 'livraria.db')
 POSTGRE = 'postgres://localhost/livraria'
+TEST_DATABASE = 'postgres://localhost/teste'
 
 class Config:
     SECRET_KEY = 'string de verificacao do flask-wtf'
@@ -22,14 +23,17 @@ class ConfigDesenvolvimento(Config):
     SERVER_NAME = 'localhost:5000'
     DEBUG = True
 
+
 class ConfigTestes(Config):
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or POSTGRE
+    SQLALCHEMY_DATABASE_URI = TEST_DATABASE
     WTF_CSRF_ENABLED = False
+
 
 class ConfigProducao(Config):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or POSTGRE
     SECRET_KEY = os.environ.get('SECRET_KEY')
+
 
 config = {
     'base': Config,
